8f474bc2e2d24055e81e2f8d25145169e8072397
art <art>
2023-10-06 12:59:25 +0300

    update

--- build-here_nopy_dbg.bat
+++ build-here_nopy_dbg.bat
@@ -22,17 +22,10 @@ if exist opencv_contrib (
     git clone --depth 1 --recursive https://github.com/opencv/opencv_contrib.git
 )
 
-@REM if exist onnx (
-
-@REM ) else (
-@REM     git clone --depth 1 --tag v1.14.1 https://github.com/onnx/onnx.git
-@REM )
-
 rmdir /s /q build
-rmdir /s /q opencv-install-nopy
 echo "Begin cmake"
 mkdir build
 cd build
 call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
-cmake  -G "Ninja" ../opencv -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DWITH_CUDA=ON -DWITH_OPENMP=ON -DWITH_OPENGL=ON -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=../opencv-install-nopy -DCMAKE_BUILD_TYPE=Release -DWITH_CUBLAS=ON -DOPENCV_DNN_CUDA=ON -DENABLE_FAST_MATH=ON  -DBUILD_opencv_python3=OFF  -DCUDA_ARCH_BIN=5.2,6.1,7.0,7.5,8.6,8.7 -DBUILD_opencv_world=ON -DWITH_ONNX=ON
+cmake  -G "Ninja" ../opencv -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DWITH_CUDA=ON -DWITH_OPENMP=ON -DWITH_OPENGL=ON -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=../opencv-install-nopy -DCMAKE_BUILD_TYPE=Debug -DWITH_CUBLAS=ON -DOPENCV_DNN_CUDA=ON -DENABLE_FAST_MATH=ON  -DBUILD_opencv_python3=OFF  -DCUDA_ARCH_BIN=5.2,6.1,7.0,7.5,8.6,8.7 -DBUILD_opencv_world=ON  -DWITH_ONNX=ON
 cmake --build . --target all install
